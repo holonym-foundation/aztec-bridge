4e230a8e361759e0bf39b9bec8d5f2e4cefce876
Muzzamil Mushtaq <muzzamilmushtaq1@gmail.com>
2025-04-08 02:19:05 +0500

    Refactor import path for ERC20 contract in PortalERC20.sol to use updated OpenZeppelin package structure.

--- l1-contracts/src/PortalERC20.sol
+++ l1-contracts/src/PortalERC20.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@oz/token/ERC20/ERC20.sol";
 
 contract PortalERC20 is ERC20 {
     constructor() ERC20("Portal", "PORTAL") {}

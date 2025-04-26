68953d4a4531fd1cc333cde5aab2583b99c0cbdf
Muzzamil Mushtaq <muzzamilmushtaq1@gmail.com>
2025-04-26 19:00:14 +0500

    add aztec sbt + deploy contracts on testnet

---
+++ bridge-script/deploy-account.ts
@@ -0,0 +1,40 @@
+
+import { AccountWallet, CompleteAddress, createLogger, Fr, PXE, waitForPXE, createPXEClient, Logger, getWallet, AccountWalletWithSecretKey } from "@aztec/aztec.js";
+import { getSchnorrAccount } from '@aztec/accounts/schnorr';
+import { deriveSigningKey } from '@aztec/stdlib/keys';
+import { SponsoredFeePaymentMethod } from "@aztec/aztec.js/fee/testing";
+import { SponsoredFPCContract } from "@aztec/noir-contracts.js/SponsoredFPC";
+import { getSponsoredFPCInstance } from "./utils/sponsored_fpc.js";
+
+const setupPXE = async () => {
+    const { PXE_URL = 'http://localhost:8081' } = process.env;
+    const pxe = await createPXEClient(PXE_URL);
+    await waitForPXE(pxe);
+    return pxe;
+};
+
+export async function deploySchnorrAccount(): Promise<AccountWalletWithSecretKey> {
+
+    let pxe: PXE;
+    let logger: Logger;
+    logger = createLogger('aztec:aztec-starter');
+
+    pxe = await setupPXE();
+
+    let secretKey = Fr.random();
+    let salt = Fr.random();
+    let schnorrAccount = await getSchnorrAccount(pxe, secretKey, deriveSigningKey(secretKey), salt);
+    let wallet = await schnorrAccount.getWallet();
+
+    const sponseredFPC = await getSponsoredFPCInstance();
+    await pxe.registerContract({instance: sponseredFPC, artifact: SponsoredFPCContract.artifact});
+    const paymentMethod = new SponsoredFeePaymentMethod(sponseredFPC.address);
+
+    let tx = await schnorrAccount.deploy({ fee: { paymentMethod } }).wait();
+
+    logger.info(`Schnorr account deployed at: ${wallet.getAddress()}`);
+
+    return wallet;
+}
+
+deploySchnorrAccount();
---
+++ bridge-script/getBlock.ts
@@ -0,0 +1,21 @@
+
+import { PXE, waitForPXE, createPXEClient } from "@aztec/aztec.js";
+
+const setupPXE = async () => {
+    const { PXE_URL = 'http://localhost:8081' } = process.env;
+    const pxe = await createPXEClient(PXE_URL);
+    await waitForPXE(pxe);
+    return pxe;
+};
+
+async function main() {
+
+    let pxe: PXE;
+    pxe = await setupPXE();
+
+    let block = await pxe.getBlock(1);
+    console.log(block)
+    console.log(await block?.hash())
+}
+
+main();

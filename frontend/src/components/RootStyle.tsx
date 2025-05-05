070c037992e1f62e8a3a6a9a34252c2630cda77f
Muzzamil Mushtaq <muzzamilmushtaq1@gmail.com>
2025-05-06 00:50:35 +0500

    Update RootStyle component to set a fixed height of 650px for the main container.

--- frontend/src/components/RootStyle.tsx
+++ frontend/src/components/RootStyle.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function RootStyle({ children, }: { children: React.ReactNode }) {
   return (
     <div className='flex items-center min-h-[100vh] justify-center'>
-      <div className='w-[380px] relative font-inter bg-latest-grey-200 rounded-lg'>
+      <div className='w-[380px] relative font-inter bg-latest-grey-200 rounded-lg h-[650px]'>
         {children}
       </div>
     </div>
